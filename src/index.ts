b805d7f7c7d2abccc2a080fe08eb321f4a156eb1
Bariq Mbani <bariq.m70@gmail.com>
2020-07-11 18:13:50 +0700

    Applying routes. Serve static folder

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import { createConnection } from 'typeorm'
 import * as dotenv from 'dotenv'
 import * as express from 'express'
 import { Application } from 'express'
+import { join } from 'path'
 
-dotenv.config()
+// Importing routes
+import PasienRoute from './route/PasienRoute'
+import AuthRoute from './route/AuthRoute'
+import RiwayatKesehatanRoute from './route/RiwayatKesehatanRoute'
+
+// Load .env file
+dotenv.config({ path: '.env' })
 
 createConnection()
   .then(async (conn) => {
@@ -13,7 +20,18 @@ createConnection()
     const PORT = process.env.PORT || 5000
 
     const app: Application = express()
+
+    // Applying middlewares
     app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+
+    // Serve express static folder
+    app.use('/public', express.static(join(__dirname, '../public')))
+
+    // Appliying routes
+    app.use('/api/v1/pasien', PasienRoute)
+    app.use('/api/v1/auth', AuthRoute)
+    app.use('/api/v1/riwayat-kesehatan', RiwayatKesehatanRoute)
 
     // Run express server
     app.listen(PORT, () => {
