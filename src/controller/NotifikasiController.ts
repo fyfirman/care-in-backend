fad46be959a78cf0002d44aa22ecaf49f15ee04f
Firmansyah Yanuar <fyfirman@gmail.com>
2020-08-15 04:41:30 +0700

    send notificatioon to fcm

--- src/controller/NotifikasiController.ts
+++ src/controller/NotifikasiController.ts
@@ -1,13 +1,39 @@
 import { Request, Response } from 'express'
 import responseLogger from '../util/responseLogger'
+import * as admin from 'firebase-admin'
 
 export const createNotifikasi = async (req: Request, res: Response) => {
   try {
-    if (!(req.body.token && req.body.title && req.body.message))
+    if (!(req.body.token && req.body.title && req.body.body))
       throw new Error('Data tidak boleh kosong')
 
-    responseLogger(req.method, 201, req.baseUrl + req.path)
-    res.status(201).json({ success: true, message: 'Berhasil mengirim notifikasi' })
+    const payload = {
+      notification: {
+        title: req.body.title,
+        body: req.body.body
+      }
+    }
+
+    admin.messaging().sendToDevice(req.body.token, payload)
+      .then((response) => {
+        if (response.results[0].error) {
+          responseLogger(req.method, 400, req.baseUrl + req.path, "Firebase error")
+          res.status(400).json({ success: false, error: response.results })
+        } else {
+          responseLogger(req.method, 200, req.baseUrl + req.path)
+          res.status(201).json({
+            success: true,
+            message: `Berhasil mengirim notifikasi.`,
+            payload,
+            response
+          })
+        }
+      }
+      ).catch((error) => {
+        responseLogger(req.method, 400, req.baseUrl + req.path, error.message)
+        res.status(400).json({ success: false, message: error.message })
+      })
+
   } catch (err) {
     let statusCode = 500
 
